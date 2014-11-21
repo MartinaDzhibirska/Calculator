59fe01f8a39940a2b00926ff8266865afcab8a96
MartinaDzhibirska <martina.dzhibirska@gmail.com>
2014-11-21 12:22:01 +0200

    Calculator
    Simple WPF Calculator made in C#

---
+++ CalculatorViewModel.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnNotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #region Private Fields
+
+        private bool _isCalculated = false;
+        private string _midResult = string.Empty;
+        private string _endResult = string.Empty;
+        private string _displayText = "0";
+        private string _operation = string.Empty;
+
+        #endregion
+
+        #region Public Fields
+
+        public bool IsCalculated
+        {
+            get
+            {
+                return _isCalculated;
+            }
+            set
+            {
+                _isCalculated = value;
+                this.OnNotifyPropertyChanged("IsCalculated");
+            }
+        }
+
+        public string MidResult
+        {
+            get
+            {
+                return _midResult;
+            }
+            set
+            {
+                _midResult = value;
+                this.OnNotifyPropertyChanged("MidResult");
+            }
+        }
+
+        public string EndResult
+        {
+            get
+            {
+                return _endResult;
+            }
+            set
+            {
+                _endResult = value;
+                this.OnNotifyPropertyChanged("EndResult");
+            }
+        }
+
+        public string DisplayText
+        {
+            get
+            {
+                return _displayText;
+            }
+            set
+            {
+                _displayText = value;
+                this.OnNotifyPropertyChanged("DisplayText");
+            }
+        }
+
+        public string Operation
+        {
+            get
+            {
+                return _operation;
+            }
+            set
+            {
+                _operation = value;
+                this.OnNotifyPropertyChanged("Operation");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void ShowNumber(string number)
+        {
+            if (DisplayText.Length > 14)
+                return;
+            if (String.IsNullOrEmpty(number))
+                DisplayText = string.Empty;             
+            else
+            {
+                if (IsCalculated)
+                {
+                    DisplayText = string.Empty;
+                    IsCalculated = false;
+                }
+                if (DisplayText == "0")
+                    DisplayText = string.Empty;
+                DisplayText += number;
+            }              
+        }
+     
+        public void Calculate(string action)
+        {
+            MidResult = DisplayText;
+            DisplayText = string.Empty;
+            Operation = action;
+            switch (action)
+            {
+                case "+":
+                    Add();
+                    break;
+                case "-":
+                    Subtract(); break;
+                case "/":
+                    Divide(); break;
+                case "*":
+                    Multiply(); break;
+                case "^":
+                    Power(); break;
+                case "S":
+                    SquareRoot(); break;
+                default:
+                    break;
+            }
+        }
+
+        public void Add()
+        {
+            if (!String.IsNullOrEmpty(MidResult))
+            {
+                if (!String.IsNullOrEmpty(EndResult))
+                {
+                    EndResult = (decimal.Parse(EndResult) + decimal.Parse(MidResult)).ToString();
+                    MidResult = string.Empty;
+                    IsCalculated = true;
+                    DisplayText = EndResult;
+                }
+                else
+                {
+                    EndResult = MidResult;
+                    MidResult = string.Empty;
+                }
+            }
+        }
+
+        public void Subtract()
+        {
+            if (!String.IsNullOrEmpty(MidResult))
+            {
+                if (!String.IsNullOrEmpty(EndResult))
+                {
+                    EndResult = (decimal.Parse(EndResult) - decimal.Parse(MidResult)).ToString();
+                    MidResult = string.Empty;
+                    IsCalculated = true;
+                    DisplayText = EndResult;
+                }
+                else
+                {
+                    EndResult = MidResult;
+                    MidResult = string.Empty;
+                }
+            }
+        }
+
+        public void Multiply()
+        {
+            if (!String.IsNullOrEmpty(MidResult))
+            {
+                if (!String.IsNullOrEmpty(EndResult))
+                {
+                    EndResult = (decimal.Parse(EndResult) * decimal.Parse(MidResult)).ToString();
+                    MidResult = string.Empty;
+                    IsCalculated = true;
+                    DisplayText = EndResult;
+                }
+                else
+                {
+                    EndResult = MidResult;
+                    MidResult = string.Empty;
+                }
+            }
+        }
+
+        public void Divide()
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(MidResult))
+                {
+                    if (!String.IsNullOrEmpty(EndResult))
+                    {
+                        EndResult = (decimal.Parse(EndResult) / decimal.Parse(MidResult)).ToString();
+                        MidResult = string.Empty;
+                        IsCalculated = true;
+                        DisplayText = EndResult;
+                    }
+                    else
+                    {
+                        EndResult = MidResult;
+                        MidResult = string.Empty;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                DisplayText = "Cannot divide by 0";
+                MidResult = string.Empty;
+                EndResult = string.Empty;
+                IsCalculated = false;
+            }
+            
+        }
+
+        public void Power()
+        {
+            if (!String.IsNullOrEmpty(MidResult))
+            {
+                if (!String.IsNullOrEmpty(EndResult))
+                {
+                    EndResult = Math.Pow(double.Parse(EndResult), double.Parse(MidResult)).ToString();
+                    MidResult = string.Empty;
+                    IsCalculated = true;
+                    DisplayText = EndResult;
+                }
+                else
+                {
+                    EndResult = MidResult;
+                    MidResult = string.Empty;
+                }
+            }
+        }
+
+        public void SquareRoot()
+        {
+           if (!String.IsNullOrEmpty(MidResult))
+            {
+                if (!String.IsNullOrEmpty(EndResult))
+                {
+                    EndResult = Math.Sqrt(double.Parse(MidResult)).ToString();
+                    MidResult = string.Empty;
+                    IsCalculated = true;
+                    DisplayText = EndResult;
+                }
+                else
+                {
+                    EndResult = Math.Sqrt(double.Parse(MidResult)).ToString();
+                    MidResult = string.Empty;
+                    IsCalculated = true;
+                    DisplayText = EndResult;
+                }
+                EndResult = string.Empty;
+            }
+        }
+
+        public void Equals()
+        {
+            if (Operation != "S")
+            {
+                Calculate(Operation);
+                IsCalculated = false;
+                EndResult = string.Empty;
+            }
+
+        }
+
+        public void Clear()
+        {
+            DisplayText = "0";
+            MidResult = string.Empty;
+            EndResult = string.Empty;
+            IsCalculated = false;
+        }
+
+        #endregion
+    }
+}
---
+++ TheCalculator.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for Calculator.xaml
+    /// </summary>
+    public partial class TheCalculator : Window
+    {
+        CalculatorViewModel calcViewModel = new CalculatorViewModel();
+        public TheCalculator()
+        {
+            InitializeComponent();
+            this.DataContext = calcViewModel;
+        }
+
+        /// <summary>
+        /// Event Handlers calling the Calculate method when an operation button is clicked.
+        /// </summary>
+       
+        #region Operations
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Calculate("+");
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Calculate("/");
+        }       
+
+        private void Substract_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Calculate("-");
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Equals();
+        }  
+
+        private void SquareRoot_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Calculate("S");
+        }
+
+        private void Power_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Calculate("^");
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Calculate("*");
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.Clear();
+        }       
+        
+        #endregion
+
+        /// <summary>
+        /// Event handlers calling the ShowNumber method when a number button is clicked.
+        /// </summary>
+  
+        #region NumberEvents
+
+        private void NumberZero_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("0");
+        }
+
+        private void NumberOne_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("1");
+        }
+
+        private void NumberTwo_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("2");
+        }
+
+        private void NumberThree_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("3");
+        }
+
+        private void NumberFour_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("4");
+        }
+
+        private void NumberFive_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("5");
+        }
+
+        private void NumberSix_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("6");
+        }
+
+        private void NumberSeven_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("7");
+        }
+
+        private void NumberEight_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("8");
+        }
+
+        private void NumberNine_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber("9");
+        }
+
+        private void Point_Click(object sender, RoutedEventArgs e)
+        {
+            calcViewModel.ShowNumber(".");
+        }
+
+        #endregion
+        
+    }
+}
